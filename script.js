85ebca0bc231b37df401d684d91785d67e36fbfc
el-chris1 <727cdiazsolchaga@frhsd.com>
2026-02-04 09:31:31 -0500

    day 2

---
+++ script.js
@@ -0,0 +1,18 @@
+let computerScore = 0;
+let playerScore = 0;    
+let computerChoice
+const choices = ['rock', 'paper', 'scissors'];
+// rock = 0 paper = 1 scissors = 2
+function getComputerChoice() {
+Math.floor(Math.random() * 3)
+}
+
+ if computerChoice === 0
+if playscore is === 2
+
+else getComputerChoice === 1
+
+if computerScore === 3 || playerScore === <3 {
+    return 'computer Wins!'
+}
+
